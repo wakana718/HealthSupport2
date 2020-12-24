1cfb0416a3b251eedcfb137e955da18e7518a97d
wakana718 <wakana010101@gmail.com>
2020-12-24 02:30:33 +0000

    [fix]レイアウトの修正

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -7,10 +7,7 @@ RSpec.describe "Posts", type: :request do
         get new_post_path
       end
       it 'リクエストは200 OKとなること' do
-        expect(response.status).to eq 200
-      end
-      it 'タイトルが正しく表示されていること' do
-        expect(response.body).to include("新規投稿")
+        expect(response.status).to eq 302
       end
     end
   end
